2bcaa77127c2b6bc5490931f14456b39e90707e4
m-nyberg <marienyberg07@gmail.com>
2025-03-14 09:20:38 +0100

    navbar

---
+++ script.js
@@ -0,0 +1,8 @@
+function klikk() {
+    const dropdownElm = document.querySelector(".dropdown-content")
+    if (dropdownElm.style.display == "flex") {
+        dropdownElm.style.display = "none"
+    } else {
+        dropdownElm.style.display = "flex"
+    }
+}
